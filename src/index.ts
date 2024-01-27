e3eaafa600f1da925de3d13537af46a525979df2
0xrinegade <0xrinegade@gmail.com>
2024-01-27 23:24:36 +0100

    fix persistence

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Bot, session, GrammyError, HttpError } from "grammy";
+import { Bot, session, GrammyError, HttpError, Context } from "grammy";
 
 import { VercelRequest, VercelResponse } from '@vercel/node';
 import { development, production } from './core';
@@ -11,15 +11,17 @@ import { SuiApi } from "./chains/sui";
 import { conversations, createConversation } from "@grammyjs/conversations";
 import { RedisAdapter } from '@grammyjs/storage-redis';
 import { kv as instance } from "@vercel/kv";
-import { UpstashRedisAdapter } from "./adapters/UpstashRedisAdapter";
+//import { UpstashRedisAdapter } from "./adapters/UpstashRedisAdapter";
 
-// if (instance && instance.opts) {
-//   instance.opts.automaticDeserialization = false;
-// }
+if (instance && instance['opts']) {
+  instance['opts'].automaticDeserialization = false
+}
 
 const BOT_TOKEN = process.env.BOT_TOKEN || '';
 const ENVIRONMENT = process.env.NODE_ENV || '';
-const storage = new UpstashRedisAdapter({ redis: instance, ttl: 10 });
+
+// @ts-ignore
+const storage = new RedisAdapter({ instance });
 
 const router = initRouter();
 const bot = new Bot<BotContext>(BOT_TOKEN);
@@ -39,7 +41,6 @@ bot.use(session({
   },
   storage
 }));
-
 
 bot.use(conversations());
 
