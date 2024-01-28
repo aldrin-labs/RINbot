e6eb413a44c0b1812f9174e987378063746b0de4
zeroxkitakitsune <kitakitsune@kitakitsune.dev>
2024-01-28 14:57:12 -0500

    deployment

--- api/webhook.ts
+++ api/webhook.ts
@@ -1,7 +1,5 @@
 import { bot } from '../src/index'
 import { webhookCallback } from 'grammy'
 
-export const config = { runtime: 'edge' }
-
 // webhookCallback will make sure that the correct middleware(listener) function is called
-export default webhookCallback(bot, 'std/http')
+export default webhookCallback(bot, 'http')
