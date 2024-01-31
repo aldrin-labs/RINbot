807f3177e66c23b230a726a559c9a19c2e1bb042
zeroxkitakitsune <kitakitsune@kitakitsune.dev>
2024-01-31 06:59:03 -0500

    check if env local

--- src/index.ts
+++ src/index.ts
@@ -21,6 +21,7 @@ const storage = new RedisAdapter({ instance });
 
 const bot = new Bot<BotContext>(BOT_TOKEN);
 
+if(ENVIRONMENT !== 'local')
 void bot.api.setWebhook(`${VERCEL_URL}/api/webhook`)
 
 async function startBot(): Promise<void> {
