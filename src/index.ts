99a3c40eeafa2d339825b6e12628209cc0716d62
zeroxkitakitsune <kitakitsune@kitakitsune.dev>
2024-01-30 21:05:26 -0500

    deployment test

--- src/index.ts
+++ src/index.ts
@@ -24,6 +24,8 @@ const webhook = `https://rinbot-dev.vercel.app/api/index`
 
 void bot.api.setWebhook(webhook)
 
+
+
 async function startBot(): Promise<void> {
   const suiApi = await (await SuiApiSingleton.getInstance()).getApi(); // Get SuiApiSingleton instance
 
