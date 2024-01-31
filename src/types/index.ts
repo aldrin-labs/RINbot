4796374459ff9a6b57609ed7a869dfcfe91df9da
avernikoz <avernikoz@gmail.com>
2024-01-31 23:36:26 +0300

    upd types & prettify

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,16 +1,22 @@
-import { Context, SessionFlavor } from "grammy";
-import { type Conversation, type ConversationFlavor, conversations, createConversation } from "@grammyjs/conversations";
-
-
+import { Context, SessionFlavor } from 'grammy';
+import {
+  type Conversation,
+  type ConversationFlavor,
+  conversations,
+  createConversation,
+} from '@grammyjs/conversations';
+import { CoinAssetData } from '@avernikoz/rinbot-sui-sdk';
 
 export interface SessionData {
-    step: "main" | "buy" | "positions" | "wallet" | "wallet-deposit"; // which step of the form we are on
-    privateKey: string,
-    publicKey: string
-    settings: { slippagePercentage: number }
+  step: 'main' | 'buy' | 'positions' | 'wallet' | 'wallet-deposit'; // which step of the form we are on
+  privateKey: string;
+  publicKey: string;
+  settings: { slippagePercentage: number };
+  assets: CoinAssetData[];
 }
 
-export type BotContext = Context & SessionFlavor<SessionData> & ConversationFlavor;
+export type BotContext = Context &
+  SessionFlavor<SessionData> &
+  ConversationFlavor;
 
 export type MyConversation = Conversation<BotContext>;
-
