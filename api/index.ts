6f48219120143ed970455259f660b1168fa182b5
root <root@core>
2024-01-30 21:01:42 -0500

    deployment test

--- api/index.ts
+++ api/index.ts
@@ -1,13 +1,5 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
-import { startVercel } from '../src';
+import { startVercel, bot } from '../src';
+import { webhookCallback } from 'grammy';
 
-export default async function handle(req: VercelRequest, res: VercelResponse) {
-  try {
-    await startVercel(req, res);
-  } catch (e: any) {
-    res.statusCode = 500;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<h1>Server Error</h1><p>Sorry, there was a problem</p>');
-    console.error(e.message);
-  }
-}
+export default webhookCallback(bot, "http");
