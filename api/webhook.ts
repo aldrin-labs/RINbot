cb0023f9000adb51fe387000e5f895eb33d1e694
avernikoz <avernikoz@gmail.com>
2024-02-03 02:51:38 +0300

    add TIMEOUT_GRAMMY_WEBHOOK to webhook

--- api/webhook.ts
+++ api/webhook.ts
@@ -1,8 +1,14 @@
 
 import { bot} from '../src'
 import { webhookCallback } from 'grammy'
-
-
+import { TIMEOUT_GRAMMY_WEBHOOK } from '../src/config/timeout.config'
 
 // webhookCallback will make sure that the correct middleware(listener) function is called
-export default webhookCallback(bot, 'http')
+export default webhookCallback(
+    bot,
+    'http',
+    (...args) => {
+        console.debug('[webhookCallback] timed out')
+    },
+    TIMEOUT_GRAMMY_WEBHOOK
+)
