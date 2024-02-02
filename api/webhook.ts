81a4ad673317b935439610f603013b4da2469c80
zeroxkitakitsune <kitakitsune@kitakitsune.dev>
2024-02-02 12:37:43 -0500

    deployment

--- api/webhook.ts
+++ api/webhook.ts
@@ -1,5 +1,8 @@
-import { bot } from '../src'
+
+import { bot} from '../src'
 import { webhookCallback } from 'grammy'
+
+
 
 // webhookCallback will make sure that the correct middleware(listener) function is called
 export default webhookCallback(bot, 'http')
