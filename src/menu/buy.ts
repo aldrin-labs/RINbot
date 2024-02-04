e7041d8692a2de6b6b4e2060413bfb50321d8e62
zeroxkitakitsune <kitakitsune@kitakitsune.dev>
2024-02-04 15:05:04 -0500

    back close btn

--- src/menu/buy.ts
+++ src/menu/buy.ts
@@ -3,7 +3,7 @@ import { BotContext } from '../types';
 import { Menu } from '@grammyjs/menu';
 // import SuiApiSingleton from '../chains/sui';
 
-const buy_menu = new Menu<BotContext>('buy-menu').text('Close', async (ctx) => {
+const buy_menu = new Menu<BotContext>('buy-menu').back('Close', async (ctx) => {
   await ctx.conversation.exit();
   // const last_msg = ctx.msg?.message_id as number;
   // await ctx.deleteMessages([last_msg]);
