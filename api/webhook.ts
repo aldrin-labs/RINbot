59d8083444eb76e583bcd2647fc8e5c339062e5f
zeroxkitakitsune <kitakitsune@kitakitsune.dev>
2024-02-05 20:53:52 -0500

    edge function

--- api/webhook.ts
+++ api/webhook.ts
@@ -3,10 +3,12 @@ import { bot} from '../src'
 import { webhookCallback } from 'grammy'
 import { TIMEOUT_GRAMMY_WEBHOOK } from '../src/config/timeout.config'
 
+export const config = { runtime: 'edge' }
+
 // webhookCallback will make sure that the correct middleware(listener) function is called
 export default webhookCallback(
     bot,
-    'http',
+    'std/http',
     (...args) => {
         console.debug('[webhookCallback] timed out')
     },
