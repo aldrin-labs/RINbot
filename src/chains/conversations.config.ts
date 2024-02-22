529514dc64a5d55cb5d036daa9fa3713464f73f7
avernikoz <avernikoz@gmail.com>
2024-02-22 03:50:21 +0300

    add create pool & create coin & list pools integrations

---
+++ src/chains/conversations.config.ts
@@ -0,0 +1,8 @@
+export enum ConversationId {
+  Buy = 'buy',
+  Sell = 'sell',
+  Withdraw = 'withdraw',
+  ExportPrivateKey = 'exportPrivateKey',
+  CreatePool = 'createPool',
+  CreateCoin = 'createCoin',
+}
