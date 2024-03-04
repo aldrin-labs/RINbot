dc0cad4e42e0cfffd9360fa2e5bb637a2f4c19c0
avernikoz <avernikoz@gmail.com>
2024-03-04 20:47:11 +0300

    change to public sui mainnet rpc

--- src/chains/sui.config.ts
+++ src/chains/sui.config.ts
@@ -1,4 +1,4 @@
 export const SUI_PROVIDER_URL =
-'https://sui-mainnet.blockvision.org/v1/2bYB32LD0S06isRTaxATLv1mTt6';
+'https://fullnode.mainnet.sui.io';
 
 export const SUI_LIQUIDITY_PROVIDERS_CACHE_OPTIONS = { updateIntervalInMs: 1000 * 60 * 60 };
