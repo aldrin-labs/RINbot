07367204f84273a77b9a25b08718484f3cf0751a
avernikoz <avernikoz@gmail.com>
2024-03-06 00:46:47 +0300

    WARNING: THIS IS TEMPORAL SWITCH TO TESTNET, MAKE SURE THIS COMMIT EITHER REVERTED OR CHANGES REMOVED BEFORE MOVING TO PROD

--- src/chains/sui.config.ts
+++ src/chains/sui.config.ts
@@ -1,4 +1,5 @@
-export const SUI_PROVIDER_URL =
-'https://fullnode.mainnet.sui.io';
+export const SUI_PROVIDER_URL = 'https://fullnode.testnet.sui.io';
 
-export const SUI_LIQUIDITY_PROVIDERS_CACHE_OPTIONS = { updateIntervalInMs: 1000 * 60 * 60 };
+export const SUI_LIQUIDITY_PROVIDERS_CACHE_OPTIONS = {
+  updateIntervalInMs: 1000 * 60 * 60,
+};
