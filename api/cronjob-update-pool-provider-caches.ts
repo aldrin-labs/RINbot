fd91818bb3e13a25fd65bd83c52a50f5f6031117
avernikoz <avernikoz@gmail.com>
2024-03-10 18:39:30 +0300

    add log

--- api/cronjob-update-pool-provider-caches.ts
+++ api/cronjob-update-pool-provider-caches.ts
@@ -3,6 +3,7 @@ import UpdatePoolProviderCaches from "../cronjobs/update-pool-provider-caches"
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   if (req && req.headers !== undefined && req.headers["Authorization"] !== `Bearer ${process.env.CRON_SECRET}`) {
+    console.debug("req.headers.Authorization", req.headers["Authorization"])
     return res.status(401).end('Unauthorized');
   }
 
