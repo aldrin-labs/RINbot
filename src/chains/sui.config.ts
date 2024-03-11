a50262996feb1fec201a8ec78c904c7367c5584e
avernikoz <avernikoz@gmail.com>
2024-03-11 16:40:29 +0300

    add usdc coin type to sui config

--- src/chains/sui.config.ts
+++ src/chains/sui.config.ts
@@ -3,3 +3,12 @@ export const SUI_PROVIDER_URL = 'https://sui-mainnet.blastapi.io/55fade13-b920-4
 export const SUI_LIQUIDITY_PROVIDERS_CACHE_OPTIONS = {
   updateIntervalInMs: 1000 * 60 * 60,
 };
+
+export const USDC_COIN_TYPE =
+  '0x5d4b302506645c37ff133b98c4b50a5ae14841659738d6d733d59d0d217a93bf::coin::COIN';
+
+// TODO: Add USDC decimals here
+
+// Note: This amount has taken from Jupiter.
+// TODO: Research and fix in case it's required.
+export const MIN_DCA_BASE_AMOUNT = '2';
