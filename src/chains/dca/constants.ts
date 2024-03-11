1bada1f9ed7b19911593ed4f316240c26e7439ca
bathord <alutskin@bk.ru>
2024-03-12 00:45:11 +0300

    Create DCA constants file & move MIN_DCA_BASE_AMOUNT to it

---
+++ src/chains/dca/constants.ts
@@ -0,0 +1,6 @@
+export const MAX_TOTAL_ORDERS_COUNT = 1_000_000;
+export const MIN_TOTAL_ORDERS_COUNT = 2;
+
+// Note: This amount has taken from Jupiter.
+// TODO: Research and fix in case it's required.
+export const MIN_DCA_BASE_AMOUNT = '2';
