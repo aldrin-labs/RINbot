fc4b8b2e135d6b4c7f9870abc66b594689ebd07c
zeroxkitakitsune <kitakitsune@kitakitsune.dev>
2024-03-10 21:26:25 -0400

    deposit bonus function

--- src/chains/sui.functions.ts
+++ src/chains/sui.functions.ts
@@ -1065,8 +1065,11 @@ export function getExplorerLink(ctx: BotContext): string {
 
 export async function home(ctx: BotContext) {
   // Send the menu.
-  if(ctx.session.bonus > 0)
+  console.log(ctx.session.bonus);
+  if(ctx.session.bonus > 0){
+    
     await depositBonus(ctx)
+  }
   const userBalance = await balance(ctx);
   const avl_balance = await availableBalance(ctx);
   const welcome_text = `<b>Welcome to RINbot on Sui Network</b>\n\nYour wallet address: <code>${ctx.session.publicKey}</code> \nYour SUI balance: <code>${userBalance}</code>\nYour available SUI balance: <code>${avl_balance}</code>`;
