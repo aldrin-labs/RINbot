48cdff8d6da5d84738293350d39ad813297d272d
zeroxkitakitsune <kitakitsune@kitakitsune.dev>
2024-03-12 18:56:32 -0400

    welcome disable

--- src/chains/welcome-bonus/welcomeBonus.ts
+++ src/chains/welcome-bonus/welcomeBonus.ts
@@ -1,7 +1,7 @@
 import { WalletManagerSingleton } from "@avernikoz/rinbot-sui-sdk";
 import yesOrNo from "../../inline-keyboards/yesOrNo";
 import { BotContext, MyConversation } from "../../types";
-import { BOT_PRIVATE_KEY, WELCOME_BONUS_AMOUNT, WELCOME_BONUS_MIN_TRADES_LIMIT } from "../../config/bot.config";
+import { BOT_PRIVATE_KEY,ENABLE_WELCOME_BONUS,WELCOME_BONUS_AMOUNT, WELCOME_BONUS_MIN_TRADES_LIMIT } from "../../config/bot.config";
 import { getTransactionFromMethod, signAndExecuteTransaction } from "../conversations.utils";
 import { retryAndGoHomeButtonsData } from "../../inline-keyboards/retryConversationButtonsFactory";
 import { ConversationId } from "../conversations.config";
@@ -13,7 +13,7 @@ export async function welcomeBonusConversation(
   ): Promise<void> {
   const retryButton = retryAndGoHomeButtonsData[ConversationId.WelcomeBonus];
   const caption = `To kickstart your journey with us, we're bestowing upon you ${conversation.session.welcomeBonus.amount} SUI to jumpstart your expedition with us. 🚀 \n\n🤔 👀 Here's the twist: After any withdrawal, your balance must remain at least ${conversation.session.welcomeBonus.amount} SUI, and the option to export your private key becomes available only after engaging in at least ${WELCOME_BONUS_MIN_TRADES_LIMIT} trades. But fear not, you're all set to trade, reap profits, and withdraw your gains!\n Are you ready to take on the challenge? \n\n🌐 Your journey begins now! Tap 'Yes' and let the trading adventure commence! 🌟`;
-  if(WELCOME_BONUS_AMOUNT === 0)
+  if(!ENABLE_WELCOME_BONUS)
     return
   const welcomeBonusInitialMessage = await ctx.replyWithPhoto('https://pbs.twimg.com/media/FttYpSnakAAS51Z.jpg', {
     caption,
