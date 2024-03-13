aa9076dd3c2a27dfb24c1a92ce0716327ff1c7e1
bathord <alutskin@bk.ru>
2024-03-13 23:01:11 +0300

    Add CloseDca to ConversationId enum

--- src/chains/conversations.config.ts
+++ src/chains/conversations.config.ts
@@ -11,5 +11,6 @@ export enum ConversationId {
   DepositDcaBase = 'depositDcaBase',
   WithdrawDcaBase = 'withdrawDcaBase',
   IncreaseDcaOrders = 'increaseDcaOrders',
+  CloseDca = 'closeDca',
   WelcomeBonus = 'welcomeBonus',
 }
