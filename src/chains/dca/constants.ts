cc836f74f749ebda38418f534074089856349f28
bathord <alutskin@bk.ru>
2024-03-13 20:39:45 +0300

    Create trade gas fee constants

--- src/chains/dca/constants.ts
+++ src/chains/dca/constants.ts
@@ -1,3 +1,6 @@
+import { SUI_DECIMALS } from '@avernikoz/rinbot-sui-sdk';
+import BigNumber from 'bignumber.js';
+
 export const MAX_TOTAL_ORDERS_COUNT = 1_000_000;
 export const MIN_TOTAL_ORDERS_COUNT = 2;
 
@@ -6,3 +9,9 @@ export const MIN_TOTAL_ORDERS_COUNT = 2;
 export const MIN_DCA_BASE_AMOUNT = '2';
 
 export const MIN_SELL_USD_PER_ORDER = 1;
+
+// TODO: Get this value from SDK
+export const ONE_TRADE_GAS_FEE_IN_MIST = 25_000_000;
+export const ONE_TRADE_GAS_FEE_IN_SUI = new BigNumber(ONE_TRADE_GAS_FEE_IN_MIST)
+  .dividedBy(10 ** SUI_DECIMALS)
+  .toString();
