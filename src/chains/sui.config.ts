830858c3399a0570b45071122c1b5c9606050e4e
bathord <alutskin@bk.ru>
2024-03-13 20:40:13 +0300

    Add RINCEL_COIN_TYPE constant

--- src/chains/sui.config.ts
+++ src/chains/sui.config.ts
@@ -8,4 +8,7 @@ export const SUI_LIQUIDITY_PROVIDERS_CACHE_OPTIONS = {
 export const USDC_COIN_TYPE =
   '0x5d4b302506645c37ff133b98c4b50a5ae14841659738d6d733d59d0d217a93bf::coin::COIN';
 
+export const RINCEL_COIN_TYPE =
+  '0xd2c7943bdb372a25c2ac7fa6ab86eb9abeeaa17d8d65e7dcff4c24880eac860b::rincel::RINCEL';
+
 // TODO: Add USDC decimals here
