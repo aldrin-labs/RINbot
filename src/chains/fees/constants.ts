ac8c0e8ec6366729815093fa02c09fb8fc372214
bathord <alutskin@bk.ru>
2024-03-15 02:24:56 +0300

    Create showFeesPage, fees constants & utils

---
+++ src/chains/fees/constants.ts
@@ -0,0 +1,7 @@
+export const DEFAULT_FEE_PERCENTAGE = 1;
+
+export const RINCEL_TO_FEES_MAP = new Map([
+  [0, DEFAULT_FEE_PERCENTAGE],
+  [10_000, 0.75],
+  [1_000_000, 0.5],
+]);
---
+++ src/chains/fees/utils.ts
@@ -0,0 +1,41 @@
+import BigNumber from 'bignumber.js';
+import { BotContext } from '../../types';
+import { RINCEL_COIN_TYPE } from '../sui.config';
+import { getWalletManager } from '../sui.functions';
+import { findCoinInAssets } from '../utils';
+import { DEFAULT_FEE_PERCENTAGE, RINCEL_TO_FEES_MAP } from './constants';
+
+export async function getUserRincelBalance(ctx: BotContext) {
+  const walletManager = await getWalletManager();
+  const allAssets = await walletManager.getAllCoinAssets(ctx.session.publicKey);
+  const foundRincel = findCoinInAssets(allAssets, RINCEL_COIN_TYPE);
+  const rincelBalance = foundRincel?.balance ?? '0';
+
+  return rincelBalance;
+}
+
+export async function getUserFeePercentage(ctx: BotContext) {
+  const rincelBalance = await getUserRincelBalance(ctx);
+  const rincelLevel = getRincelLevel(rincelBalance);
+  const feePercentage = RINCEL_TO_FEES_MAP.get(rincelLevel);
+
+  return feePercentage ?? DEFAULT_FEE_PERCENTAGE;
+}
+
+export function getRincelLevel(balance: string): number {
+  const rincelLevels = Array.from(RINCEL_TO_FEES_MAP.keys());
+  const rincelLevelsBN = rincelLevels.map((level) => new BigNumber(level));
+
+  let resultLevel: BigNumber = new BigNumber(Math.min(...rincelLevels));
+
+  rincelLevelsBN.forEach((level: BigNumber) => {
+    if (
+      level.isLessThanOrEqualTo(balance) &&
+      level.isGreaterThan(resultLevel)
+    ) {
+      resultLevel = level;
+    }
+  });
+
+  return resultLevel.toNumber();
+}
