945513a051254ca61ef77eacc74418f921f999b4
zeroxkitakitsune <kitakitsune@kitakitsune.dev>
2024-03-15 12:00:37 -0400

    exponential formatting

--- src/chains/utils.ts
+++ src/chains/utils.ts
@@ -236,7 +236,7 @@ export async function getPriceOutputData(validCoin: string | CoinAssetData) {
     if (priceApiGetResponse?.data?.data?.price) {
       price = priceApiGetResponse.data.data.price;
       if(isExponential(price))
-        price = +price.toFixed(10)
+        price = price.toFixed(10)
       return `You are selling <code>${validCoin.type}</code> for <b>$${price} USD</b> per token\n\n`;
     } else {
       // Handle case where price data is not available but the request did not fail
@@ -248,7 +248,7 @@ export async function getPriceOutputData(validCoin: string | CoinAssetData) {
     if (priceApiGetResponse?.data?.data?.price) {
       price = priceApiGetResponse.data.data.price;
       if(isExponential(price))
-        price = +price.toFixed(10)
+        price = price.toFixed(10)
       return `You are buying <code>${validCoin}</code> for <b>$${price} USD</b> per token\n\n`;
     } else {
       // Handle case where price data is not available but the request did not fail
