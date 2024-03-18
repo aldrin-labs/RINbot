df339cc5eb27074ba7eefdfed16950f15ad20d53
zeroxkitakitsune <kitakitsune@kitakitsune.dev>
2024-03-18 16:30:22 -0400

    net worth fix

--- src/chains/priceapi.utils.ts
+++ src/chains/priceapi.utils.ts
@@ -28,7 +28,7 @@ export async function postPriceApi(allCoinsAssets: CoinAssetData[]) {
 
         const startTime = Date.now();
 
-        const response = await axios.post<AxiosPriceApiResponsePost>(`${PRICE_API_URL}/assets`, data, { timeout: 400 })
+        const response = await axios.post<AxiosPriceApiResponsePost>(`${PRICE_API_URL}/assets`, data, { timeout: 1200 })
 
         const endTime = Date.now();
         const requestTime = endTime - startTime;
