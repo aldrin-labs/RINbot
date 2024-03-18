fb455698cc001204ea2774207de719775cfe2e4a
zeroxkitakitsune <kitakitsune@kitakitsune.dev>
2024-03-18 18:00:30 -0400

    net worth fix

--- src/chains/pnl.utils.ts
+++ src/chains/pnl.utils.ts
@@ -1,22 +1,22 @@
 import { Trade } from "../types";
 
 
-function calculateCumulativePnLPercentage(trades: Trade[]): string {
-    let totalBuyingCost = 0;
-    let totalSellingRevenue = 0;
-    let totalFees = 0;
+// function calculateCumulativePnLPercentage(trades: Trade[]): string {
+//     let totalBuyingCost = 0;
+//     let totalSellingRevenue = 0;
+//     let totalFees = 0;
 
-    trades.forEach(trade => {
-        totalBuyingCost += trade.buyingPrice * trade.quantity;
-        totalSellingRevenue += trade.sellingPrice * trade.quantity;
-        totalFees += trade.fees || 0;
-    });
+//     trades.forEach(trade => {
+//         totalBuyingCost += trade.buyingPrice * trade.quantity;
+//         totalSellingRevenue += trade.sellingPrice * trade.quantity;
+//         totalFees += trade.fees || 0;
+//     });
 
-    const totalCost = totalBuyingCost + totalFees;
-    const pnlPercentage = ((totalSellingRevenue - totalCost) / totalBuyingCost) * 100;
+//     const totalCost = totalBuyingCost + totalFees;
+//     const pnlPercentage = ((totalSellingRevenue - totalCost) / totalBuyingCost) * 100;
 
-    return pnlPercentage.toFixed(2) + '%';
-}
+//     return pnlPercentage.toFixed(2) + '%';
+// }
 
 
 
