8cde6c03a07b6e9584e8ae97813169e9fe38fabb
bathord <alutskin@bk.ru>
2024-03-21 00:51:26 +0300

    Add aldrin & mysten image and update text on refund page

--- src/chains/refunds/showRefundsPage.ts
+++ src/chains/refunds/showRefundsPage.ts
@@ -1,10 +1,14 @@
+import { InputFile } from 'grammy';
 import refundsMenu from '../../menu/refunds';
 import { BotContext } from '../../types';
+import { REFUND_PAGE_IMAGE_PATH } from './config';
 
 export async function showRefundsPage(ctx: BotContext) {
-  await ctx.reply(
-    '🚨 <b>Has your account been affected by the Romas Rug Pull incident?</b> 🚨\n\n' +
+  await ctx.replyWithPhoto(new InputFile(REFUND_PAGE_IMAGE_PATH), {
+    caption:
+      '🚨 <b>Has your account been affected by the Romas Rug Pull incident?</b> 🚨\n\n' +
       'Let us check it out and come with refund ways.',
-    { reply_markup: refundsMenu, parse_mode: 'HTML' },
-  );
+    reply_markup: refundsMenu,
+    parse_mode: 'HTML',
+  });
 }
