a590d1d549d8d825b9531c77bf19361ce32dbb4c
bathord <alutskin@bk.ru>
2024-03-21 18:55:11 +0300

    Remove path to stored into JSON affected addresses

--- src/chains/refunds/config.ts
+++ src/chains/refunds/config.ts
@@ -1,5 +1,2 @@
-export const REFUNDABLE_ACCOUNTS_DATA_JSON_PATH =
-  './src/storage/refundable-addresses.json';
-
 export const REFUND_PAGE_IMAGE_PATH =
   './src/images/aldrin_labs_ mysten_labs.jpg';
