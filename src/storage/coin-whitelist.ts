47e5dbdf0698a13437c3935e25ca7e4134e7f556
bathord <alutskin@bk.ru>
2024-03-27 17:25:49 +0300

    Create coin whitelist v0

---
+++ src/storage/coin-whitelist.ts
@@ -0,0 +1,58 @@
+export default [
+  {
+    symbol: 'FUD',
+    type: '0x76cb819b01abed502bee8a702b4c2d547532c12f25001c9dea795a5e631c26f1::fud::FUD',
+  },
+  {
+    symbol: 'HSUI',
+    type: '0x8c47c0bde84b7056520a44f46c56383e714cc9b6a55e919d8736a34ec7ccb533::suicune::SUICUNE',
+  },
+  {
+    symbol: 'SCB',
+    type: '0x9a5502414b5d51d01c8b5641db7436d789fa15a245694b24aa37c25c2a6ce001::scb::SCB',
+  },
+  {
+    symbol: 'CETUS',
+    type: '0x6864a6f921804860930db6ddbe2e16acdf8504495ea7481637a1c8b9a8fe54b::cetus::CETUS',
+  },
+  {
+    symbol: 'TURBOS',
+    type: '0x5d1f47ea69bb0de31c313d7acf89b890dbb8991ea8e03c6c355171f84bb1ba4a::turbos::TURBOS',
+  },
+  {
+    symbol: 'USDY',
+    type: '0x960b531667636f39e85867775f52f6b1f220a058c4de786905bdf761e06a56bb::usdy::USDY',
+  },
+  {
+    symbol: 'FLAME',
+    type: '0x247a6d271810efbe80943433e84b9360e2f976fce89e7c19dc680f5aae8738e2::flame::FLAME',
+  },
+  {
+    symbol: 'RINCEL',
+    type: '0xd2c7943bdb372a25c2ac7fa6ab86eb9abeeaa17d8d65e7dcff4c24880eac860b::rincel::RINCEL',
+  },
+  {
+    symbol: 'ETHW',
+    type: '0xc103153755336ccfc91f5524745557868103ce2454d8d473ba0e560f7e52c9e5::ethw::ETHW',
+  },
+  {
+    symbol: 'HHS',
+    type: '0x996374e9df45796fe8112663a37272af4d33e7531c67d21aaa5cc93b3d1ded5c::hhs::HHS',
+  },
+  {
+    symbol: 'KIMCHI',
+    type: '0xb6baa75577e4bbffba70207651824606e51d38ae23aa94fb9fb700e0ecf50064::kimchi::KIMCHI',
+  },
+  {
+    symbol: 'uni the wonder dog',
+    type: '0xead92ec6492b9c51ab7dbb079e60d8049062f771951df15f8012d22baeff4c93::unithewonderdog::UNITHEWONDERDOG',
+  },
+  {
+    symbol: 'VAPOREON',
+    type: '0x84d155fb70aebcc1391bf497d8fc139154be745765dfec57faef4704f4112c79::vaporeon::VAPOREON',
+  },
+  {
+    symbol: 'SUIB',
+    type: '0xed4504e791e1dad7bf93b41e089b4733c27f35fde505693e18186c2ba8e2e14b::suib::SUIB',
+  },
+];
