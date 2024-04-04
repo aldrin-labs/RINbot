edc40bee5f5ac6b7eadce751963aaca36d434f3f
avernikoz <avernikoz@gmail.com>
2024-04-05 01:32:34 +0300

    upd version to 3.0.4

--- src/index.ts
+++ src/index.ts
@@ -37,7 +37,7 @@ function errorBoundaryHandler(err: BotError) {
   console.error('[Error Boundary Handler]', err);
 }
 
-const APP_VERSION = '3.0.3';
+const APP_VERSION = '3.0.4';
 
 if (instance && instance['opts']) {
   instance['opts'].automaticDeserialization = false;
