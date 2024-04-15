56aef01c4b8b0fb61ce4e787d177d0fee314e99c
bathord <alutskin@bk.ru>
2024-04-15 14:47:19 +0300

    Add comment for conversationErrorBoundaryHandler

--- src/error-boundaries/conversations-boundary.ts
+++ src/error-boundaries/conversations-boundary.ts
@@ -6,6 +6,11 @@ import { EndConversationError } from '../errors/end-conversation.error';
  */
 export async function conversationErrorBoundaryHandler(error: BotError) {
   if (error.error instanceof EndConversationError) {
+    /**
+     * In case error is `EndConversationError`, we ignore it and let user work further, because this error is thrown
+     * only to end conversation from inside (ref: https://grammy.dev/plugins/conversations#leaving-a-conversation).
+     * This is a workaround for exiting the conversation from any its point (e.g. buy util method, etc.)
+     */
     return;
   } else {
     console.debug('[Conversation Error Boundary] Unrecognized error occured, throwing further...');
