c9c3aee35e8c14a250dbb5b0b338731e4401726a
bathord <alutskin@bk.ru>
2024-04-15 23:47:32 +0300

    Remove repeat same buy inline keyboards

--- src/inline-keyboards/trading/repeat-buy-with-home.ts
+++
@@ -1,7 +0,0 @@
-import goHome from '../goHome';
-import repeatSameBuyKeyboard from './repeat-same-buy';
-
-const homeButtons = goHome.inline_keyboard[0];
-const repeatSameBuyWithHomeKeyboard = repeatSameBuyKeyboard.clone().add(...homeButtons);
-
-export default repeatSameBuyWithHomeKeyboard;
--- src/inline-keyboards/trading/repeat-same-buy.ts
+++
@@ -1,6 +0,0 @@
-import { InlineKeyboard } from 'grammy';
-import { CallbackQueryData } from '../../types/callback-queries-data';
-
-const repeatSameBuyKeyboard = new InlineKeyboard().text('Repeat', CallbackQueryData.RepeatSameBuy);
-
-export default repeatSameBuyKeyboard;
