c1c1fbff1c98c1362a4895ac5cb9c84f72572264
bathord <alutskin@bk.ru>
2024-04-16 17:06:19 +0300

    Upd printSwapInfo() to print offchain & calc prices and difference between them

--- src/chains/trading/utils.ts
+++ src/chains/trading/utils.ts
@@ -1,8 +1,16 @@
-import { RouteManager, SHORT_SUI_COIN_TYPE, transactionFromSerializedTransaction } from '@avernikoz/rinbot-sui-sdk';
+import {
+  LONG_SUI_COIN_TYPE,
+  RouteManager,
+  SHORT_SUI_COIN_TYPE,
+  isSuiCoinType,
+  transactionFromSerializedTransaction,
+} from '@avernikoz/rinbot-sui-sdk';
 import BigNumber from 'bignumber.js';
 import confirmWithCloseKeyboard from '../../inline-keyboards/mixed/confirm-with-close';
 import { BotContext, MyConversation } from '../../types';
+import { SUI_COIN_DATA } from '../sui.config';
 import { getCoinManager, getRouteManager, randomUuid } from '../sui.functions';
+import { getCoinPrice } from '../utils';
 import { SwapSide } from './types';
 
 export async function printSwapInfo({
@@ -20,33 +28,68 @@ export async function printSwapInfo({
   providerName: string;
   side: SwapSide;
 }) {
+  const userShouldConfirmSwap = conversation.session.settings.swapWithConfirmation;
   const coinManager = await getCoinManager();
 
   const inputCoinType = side === SwapSide.Buy ? SHORT_SUI_COIN_TYPE : conversation.session.tradeCoin.coinType;
   const outputCoinType = side === SwapSide.Buy ? conversation.session.tradeCoin.coinType : SHORT_SUI_COIN_TYPE;
 
   const inputCoinData = await conversation.external(() => coinManager.getCoinByType2(inputCoinType));
-  const outputCoinData = await conversation.external(() => coinManager.getCoinByType2(outputCoinType));
+  const outputCoinData = isSuiCoinType(outputCoinType)
+    ? SUI_COIN_DATA
+    : await conversation.external(() => coinManager.getCoinByType2(outputCoinType));
 
   const outputAmountIsRaw = outputCoinData === null;
   const outputCoinDecimals = outputAmountIsRaw ? 0 : outputCoinData.decimals;
 
   const formattedOutputAmount = new BigNumber(outputAmount.toString()).div(10 ** outputCoinDecimals).toString();
 
-  const inputCoinSymbol = (inputCoinData?.symbol ?? inputCoinType).toUpperCase();
-  const outputCoinSymbol = (outputCoinData?.symbol ?? outputCoinType).toUpperCase();
+  const inputCoinSymbol = inputCoinData?.symbol?.toUpperCase() ?? inputCoinType;
+  const outputCoinSymbol = outputCoinData?.symbol?.toUpperCase() ?? outputCoinType;
 
-  // TODO: Add prices (offchain and from output amount) print
-  // const coinTypeToFetchPrice = side === SwapSide.Buy ? outputCoinType : inputCoinType;
-  // const offchainPrice = await conversation.external(() => getCoinPrice(coinTypeToFetchPrice));
-  // const offchainPriceString =
-  //   offchainPrice !== null ? `Current offchain <b>${outputCoinSymbol}</b> price: $<code>${offchainPrice}</code>\n`
-  // : '';
+  const coinTypeToPrintPrice = side === SwapSide.Buy ? outputCoinType : inputCoinType;
+  const coinSymbolToPrintPrice = side === SwapSide.Buy ? outputCoinSymbol : inputCoinSymbol;
 
-  // const calculatedPrice = side === SwapSide.Buy ? new BigNumber(outputAmount.toString()).div().toString() : ``;
+  // Getting offchain price part
+  const offchainPrice = await conversation.external(() => getCoinPrice(coinTypeToPrintPrice));
+  const offchainPriceString =
+    offchainPrice !== null
+      ? `Current offchain <b>${coinSymbolToPrintPrice}</b> price: $<code>${offchainPrice}</code>\n`
+      : '';
 
-  const userShouldConfirmSwap = conversation.session.settings.swapWithConfirmation;
+  // Getting calculated from swap price part
+  let calculatedPriceString = '';
+  let calculatedPrice: string | null = null;
 
+  if (!outputAmountIsRaw) {
+    const suiPrice = await conversation.external(() => getCoinPrice(LONG_SUI_COIN_TYPE));
+
+    if (suiPrice !== null) {
+      if (side === SwapSide.Buy) {
+        const outputCoinPriceInInputCoin = new BigNumber(formattedInputAmount).div(formattedOutputAmount);
+        const outputCoinPriceInUsd = outputCoinPriceInInputCoin.multipliedBy(suiPrice).toFixed(9);
+
+        calculatedPrice = outputCoinPriceInUsd;
+      } else {
+        const inputCoinPriceInOutputCoin = new BigNumber(formattedOutputAmount).div(formattedInputAmount);
+        const inputCoinPriceInUsd = inputCoinPriceInOutputCoin.multipliedBy(suiPrice).toFixed(9);
+
+        calculatedPrice = inputCoinPriceInUsd;
+      }
+
+      calculatedPriceString = `Current swap <b>${coinSymbolToPrintPrice}</b> price: $<code>${calculatedPrice}</code>\n`;
+    }
+  }
+
+  // Getting prices difference part
+  let pricesDifferenceString = '';
+
+  if (offchainPrice !== null && calculatedPrice !== null) {
+    const priceDifference = new BigNumber(offchainPrice).minus(calculatedPrice).abs().toFixed(9);
+    pricesDifferenceString = `Difference in prices: $<code>${priceDifference}</code>\n`;
+  }
+
+  // Getting action part
   let actionPartString = '';
 
   if (userShouldConfirmSwap) {
@@ -55,18 +98,35 @@ export async function printSwapInfo({
     actionPartString = side === SwapSide.Buy ? 'You are buying' : 'You are selling';
   }
 
+  const rawOutputAmountString = outputAmountIsRaw ? ` <i>(*raw)</i>` : '';
+
+  // Constructing result message
   let message =
     side === SwapSide.Buy
-      ? `${actionPartString} <code>${formattedOutputAmount}</code> <b>${outputCoinSymbol}</b> for ` +
-        `<code>${formattedInputAmount}</code> <b>${inputCoinSymbol}</b> using <b><i>${providerName}</i></b> liquidity provider.\n\n`
+      ? `${actionPartString} <code>${formattedOutputAmount}</code>${rawOutputAmountString} ` +
+        `<b>${outputCoinSymbol}</b> for <code>${formattedInputAmount}</code> <b>${inputCoinSymbol}</b> ` +
+        `using <b><i>${providerName}</i></b> liquidity provider.\n\n`
       : `${actionPartString} <code>${formattedInputAmount}</code> <b>${inputCoinSymbol}</b> for ` +
-        `<code>${formattedOutputAmount}</code> <b>${outputCoinSymbol}</b> using <b><i>${providerName}</i></b> liquidity provider.\n\n`;
+        `<code>${formattedOutputAmount}</code>${rawOutputAmountString} <b>${outputCoinSymbol}</b> ` +
+        `using <b><i>${providerName}</i></b> liquidity provider.\n\n`;
 
+  // Adding prices strings after general swap info
+  const pricesString = offchainPriceString + calculatedPriceString + pricesDifferenceString;
+
+  if (pricesString !== '') {
+    message += pricesString + '\n';
+  }
+
+  // Adding hint and note if user should confirm the swap
   message += userShouldConfirmSwap
     ? `<span class="tg-spoiler"><b>Hint</b>: you can switch <b><i>Swap Confirmation</i></b> in ` +
       `<b><i>Settings</i></b>.\n\n<b>Note</b>: you should confirm your swap within 15 seconds. ` +
-      `Otherwise, swap could be failed due to price changes.</span>`
+      `Otherwise, swap could be failed due to price changes.</span>\n\n`
     : '';
+
+  if (outputAmountIsRaw) {
+    message += "<i>*raw</i> — amount doesn't respect decimals.";
+  }
 
   await ctx.reply(message, {
     parse_mode: 'HTML',
