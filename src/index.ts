c056864d74da14c0b19398c5158bf39336ba2d81
avernikoz <avernikoz@gmail.com>
2024-04-16 22:57:42 +0300

    upd to 3.0.6

--- src/index.ts
+++ src/index.ts
@@ -39,7 +39,7 @@ import { removeStep } from './migrations/removeStep';
 import { documentClient } from './services/aws';
 import { BotContext, SessionData } from './types';
 
-const APP_VERSION = '3.0.5';
+const APP_VERSION = '3.0.6';
 
 if (instance && instance['opts']) {
   instance['opts'].automaticDeserialization = false;
