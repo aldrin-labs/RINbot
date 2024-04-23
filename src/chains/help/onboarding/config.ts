d5407f60c10803aec90b6a9e02eb4f5e92f9ef25
bathord <alutskin@bk.ru>
2024-04-23 14:52:24 +0300

    Create onb config

---
+++ src/chains/help/onboarding/config.ts
@@ -0,0 +1,4 @@
+import { OnboardingCallbackQueryData } from './callback-query-data';
+
+export const onboardingSectionsMenuId = 'onb-sections';
+export const firstArticleCallbackQueryData = OnboardingCallbackQueryData.PublicKey;
