eb82a7596c4de80917572e4d1ebbd950d22b23b9
bathord <alutskin@bk.ru>
2024-04-29 18:17:10 +0300

    Add referral QR-code showing

--- src/menu/referral/share.ts
+++ src/menu/referral/share.ts
@@ -1,12 +1,13 @@
 import { Menu } from '@grammyjs/menu';
 import { showReferralLinkPage } from '../../chains/referral/pages/link';
+import { showReferralQrCodePage } from '../../chains/referral/pages/qr-code';
 import { BotContext } from '../../types';
 
 export const shareMyReferralMenuId = 'share-referral';
 
 const shareMyReferralMenu = new Menu<BotContext>(shareMyReferralMenuId)
   .text('QR-code', async (ctx) => {
-    await ctx.reply('Soon...');
+    await showReferralQrCodePage(ctx);
   })
   .text('Link', async (ctx) => {
     await showReferralLinkPage(ctx);
