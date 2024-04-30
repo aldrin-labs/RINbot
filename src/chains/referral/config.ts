f62f880c38484ed60fa196c4aa1f5e0a9fb93f1a
bathord <alutskin@bk.ru>
2024-04-30 17:20:39 +0300

    Add ReferralsTrades to ReferralRedisKeyFirstPart enum

--- src/chains/referral/config.ts
+++ src/chains/referral/config.ts
@@ -11,6 +11,7 @@ export const REF_PARAM_KEY = 'ref';
 export enum ReferralRedisKeyFirstPart {
   Referrer = 'referrer',
   Referrals = 'referrals',
+  ReferralsTrades = 'referrals_trades',
 }
 
 export enum ReferrerFieldKey {
