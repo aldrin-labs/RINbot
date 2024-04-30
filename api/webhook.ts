e5375784328dc9e60e42c1bcb90cfaf67e241f11
bathord <alutskin@bk.ru>
2024-04-30 20:14:36 +0300

    Revert "Throw error on webhook callback timeout"
    This reverts commit a6bbb09875e66f2879e96370d3457c9a5d82ce9d.

--- api/webhook.ts
+++ api/webhook.ts
@@ -1,6 +1,14 @@
-import { bot } from '../src';
-import { webhookCallback } from 'grammy';
-import { TIMEOUT_GRAMMY_WEBHOOK } from '../src/config/timeout.config';
+
+import { bot} from '../src'
+import { webhookCallback } from 'grammy'
+import { TIMEOUT_GRAMMY_WEBHOOK } from '../src/config/timeout.config'
 
 // webhookCallback will make sure that the correct middleware(listener) function is called
-export default webhookCallback(bot, 'http', 'throw', TIMEOUT_GRAMMY_WEBHOOK);
+export default webhookCallback(
+    bot,
+    'http',
+    (...args) => {
+        console.debug('[webhookCallback] timed out')
+    },
+    TIMEOUT_GRAMMY_WEBHOOK
+)
