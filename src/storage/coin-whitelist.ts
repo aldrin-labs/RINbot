f041dda3b9489183f0c8064e2a72f04bc01e4635
avernikoz <avernikoz@gmail.com>
2024-05-15 23:34:21 +0300

    fix cetus coin normalization for 0x0

--- src/storage/coin-whitelist.ts
+++ src/storage/coin-whitelist.ts
@@ -30,7 +30,7 @@ export default [
   },
   {
     symbol: 'CETUS',
-    type: '0x6864a6f921804860930db6ddbe2e16acdf8504495ea7481637a1c8b9a8fe54b::cetus::CETUS',
+    type: '0x06864a6f921804860930db6ddbe2e16acdf8504495ea7481637a1c8b9a8fe54b::cetus::CETUS',
   },
   {
     symbol: 'TURBOS',
